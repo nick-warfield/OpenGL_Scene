f02dde64988fe8a96a5b881ac0287a8d36331018
Nicholas Warfield <nick@nkwarfield.io>
2020-09-11 21:17:43 -0700

    Did some preliminary transform work

--- include/Transform.hpp
+++ include/Transform.hpp
@@ -2,6 +2,8 @@
 #define TRANSFORM_HPP
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 struct Transform {
 	glm::vec3 position;
@@ -9,6 +11,14 @@ struct Transform {
 	glm::vec3 scale;
 };
 
-glm::mat4 get_matrix(Transform);
+glm::mat4 get_matrix(Transform t) {
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::translate(trans, t.position);
+	trans = glm::rotate(trans, glm::radians(t.rotation.x), glm::vec3(1, 0, 0));
+	trans = glm::rotate(trans, glm::radians(t.rotation.y), glm::vec3(0, 1, 0));
+	trans = glm::rotate(trans, glm::radians(t.rotation.z), glm::vec3(0, 0, 1));
+	trans = glm::scale(trans, t.scale);
+	return trans;
+}
 
 #endif
